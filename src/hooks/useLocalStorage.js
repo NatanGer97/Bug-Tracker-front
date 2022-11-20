664bf1966eaf516d96f9a17c79dd275f68f73a4b
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2022-11-20 17:20:14 +0200

    test if token expires was added

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react"
 import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 
 const useLocalStorage = () => {
     const[token, setToken] = useState(localStorage.getItem('token'));
+    const navigate = useNavigate();
 
     useEffect(() => 
     {
       if (token)
       {
-        var decoded = jwt_decode(token);
-        console.log(Object.entries(decoded));
-        console.log(decoded.UserInfo.roles);
-        console.log(decoded.UserInfo.username);
+        const decoded = jwt_decode(token);
+        // test if token expires
+        if (decoded.exp * 1000 < Date.now())
+        {
+          alert('token expires, please login again');
+          localStorage.removeItem('token');
+          navigate('/login');
+        }
+        
+        
         // decodedTokenInfo = {roles: decoded.UserInfo.roles, username: decoded.UserInfo.username}
         
       }
